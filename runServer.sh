3652d919d6e55717ab3a0a419d3213d12f6c0ab1
ChoiMoonSeok <mon08290@naver.com>
2022-12-27 22:20:24 +0900

    도커를 통한 서버 배포 자동화 작성

--- runServer.sh
+++ runServer.sh
@@ -1,9 +1,11 @@
-# 도커 컨테이너 및 이미지 삭제
-sudo docker rm -f $(sudo docker ps -aq)
-sudo docker rmi -f $(sudo docker ps -aq)
+# 도커 컴포즈를 활용하여 자동 배포
 
-# db_config.py 파일 보존
-sudo mv db_config.py ..
+# 도커 컨테이너 및 이미지 삭제
+sudo docker-compose down
+
+# db_config.py 파일 및 로그 보존
+sudo mv config.py ..
+sudo mv /logs/Server.log ..
 
 # 디렉토리(코드) 삭제
 cd ..
@@ -13,9 +15,9 @@ sudo rm -r CarbonServerDjango
 git clone https://github.com/ChoiMoonSeok/CarbonServerDjango.git
 
 # 디렉토리로 이동
-mv db_config.py CarbonServerDjango
+sudo mv config.py CarbonServerDjango
+sudo mv Server.log CarbonServerDjango/logs
 cd CarbonServerDjango
 
 # 도커 빌드 및 실행
-sudo docker build -t carbonserver:0.0 .
-sudo docker run -it -v $(pwd):/home carbonserver:0.0
+sudo docker-compose up --build

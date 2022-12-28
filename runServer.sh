cce6c46318d889265145a673db51bfb49c410a1a
ChoiMoonSeok <mon08290@naver.com>
2022-12-28 18:01:49 +0900

    서버 배포용 셸스크립트 수정

--- runServer.sh
+++ runServer.sh
@@ -1,22 +1,26 @@
 # 도커 컴포즈를 활용하여 자동 배포
 
 # 도커 컨테이너 및 이미지 삭제
-sudo docker-compose down
+docker-compose down
 
 # db_config.py 파일 및 로그 보존
-# sudo mv config.py ..
-# sudo mv /logs/Server.log ..
+sudo mv config.py ..
+sudo mv logs ..
+sudo mv .env ..
 
-# 디렉토리(코드) 업데이트
-git pull origin master
+# 구형 코드 삭제
+cd ..
+sudo rm -r CarbonServerDjango
 
 # 코드 가져오기 
 git clone https://github.com/ChoiMoonSeok/CarbonServerDjango.git
 
 # 디렉토리로 이동
-# sudo mv config.py CarbonServerDjango
-# sudo mv Server.log CarbonServerDjango/logs
-# cd CarbonServerDjango
+sudo mv config.py CarbonServerDjango
+sudo mv logs CarbonServerDjango
+sudo mv .env CarbonServerDjango
+
+cd CarbonServerDjango
 
 # 도커 빌드 및 실행
-sudo docker-compose up --build
+docker-compose up --build

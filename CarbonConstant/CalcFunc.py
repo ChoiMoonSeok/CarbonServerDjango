898c637b393d4561d0e58994454c96cd1e866d89
ChoiMoonSeok <mon08290@naver.com>
2023-01-09 00:13:39 +0900

    상수 정리

--- CarbonConstant/CalcFunc.py
+++ CarbonConstant/CalcFunc.py
@@ -1,4 +1,4 @@
-from .Constants import Electric
+import Constants
 
 # 탄소 배출 유형과 숫자 맵핑
 CarbonCateLen = 14
@@ -23,9 +23,28 @@ CarbonCategories = [
 # 전력 사용량의 탄소 배출량을 계산하는 함수
 def ElecUsage(usage):
     ElecCar = (
-        (usage * Electric.CO2_EF)
-        + (usage * Electric.NH4_EF * Electric.NH4_MULTIPLY_CON)
-        + (usage * Electric.N2O_EF * Electric.N2O_MULTIPLY_CON)
+        (usage * Constants.Electric.CO2_EF)
+        + (usage * Constants.Electric.NH4_EF * Constants.Electric.NH4_MULTIPLY_CON)
+        + (usage * Constants.Electric.N2O_EF * Constants.Electric.N2O_MULTIPLY_CON)
     )
 
     return ElecCar
+
+
+# 열 사용량
+def HeatUsage(usage):
+    return usage * Constants.Heat.HEAT_EF
+
+
+# 수도 사용량
+def WaterUsage(usage):
+    return usage * Constants.Water.WATER_EF
+
+
+# 탈루 배출
+# def FugitiveUsage(usage, num, type):
+#     if type == HFC:
+#         Constants.탈루배출.REFRI_HFC_134A_MULTI * R
+#     elif type == R_407C:
+
+#     else:
